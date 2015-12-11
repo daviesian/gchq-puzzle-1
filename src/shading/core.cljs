29b0c70920fba28be634501fc1ef12958e706a05
Ian Davies <git@iandavies.org>
2015-12-11 10:08:24 +0000

    Initial Commit

---
+++ src/shading/core.cljs
@@ -0,0 +1,26 @@
+(ns shading.core
+  (:require [reagent.core :as reagent :refer [atom]]))
+
+(enable-console-print!)
+
+(println "Edits to this text should show up in your developer console.")
+
+;; define your app data so that it doesn't get over-written on reload
+
+(defonce app-state (atom {:text "Hello world!"}))
+
+
+(defn reset-app-state []
+  (reset! app-state {}))
+
+(defn hello-world []
+  [:h1 (str @app-state)])
+
+(reagent/render-component [hello-world]
+                          (. js/document (getElementById "app")))
+
+
+(defn on-js-reload []
+  ;; optionally touch your app-state to force rerendering depending on
+  ;; your application
+  (swap! app-state update-in [:__figwheel_counter] inc))
